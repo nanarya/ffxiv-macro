bcf0fb9a3ffb24284d541bf53c6b775bb0fd25fe
nanarya <>
2019-06-07 21:45:55 +0900

    #1 rails g model macro

---
+++ db/migrate/20190607114939_create_macros.rb
@@ -0,0 +1,10 @@
+class CreateMacros < ActiveRecord::Migration[5.2]
+  def change
+    create_table :macros do |t|
+      t.string :title
+      t.text :body
+
+      t.timestamps
+    end
+  end
+end
